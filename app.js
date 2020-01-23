144c53ebdb9862a17e3e2a1840eec811e3d02942
Corina Pelichowski <corina@danitt.com>
2020-01-23 22:55:06 +1000

    Primitive and Reference types

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert("hello");
+// PRIMITIVE
+
+// String
+const name = "John Doe";
+// Number
+const age = 45;
+// Boolean
+const hasKids = false;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+
+// REFERENCE TYPES
+
+// Array
+const hobbies = ['movies', 'music',];
+// Object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
