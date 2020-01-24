c49cbd4bb91484e5ba5410c0579d2f8a940dd2fb
Corina Pelichowski <corina@danitt.com>
2020-01-24 22:54:24 +1000

    numbers & the Math Object

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
+const num1 = 100;
+const num2 = 50;
 let val;
 
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// Number to String
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 41, 1, 55, 6, 3, -2);
+val = Math.max(2, 33, 41, 1, 55, 6, 3, -2);
+val = Math.random();
 
-val = String(5);
-val = String(4 + 4);
-// Boolean to String
-val = String(true);
-// Date to String
-val = String(new Date());
-// Array to String
-val = String([1, 2, 3, 4])
 
-// toString()
-val = (5).toString();
-val = (true).toString();
-
-// String to Number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1, 2, 3, 4]);
-
-val = parseInt('100.30');
-val = parseFloat('100.3');
-
-// Output 
+val = Math.floor(Math.random() * 20 + 1);
 console.log(val);
-console.log(typeof val);
-//console.log(val.length);
-console.log(val.toFixed());
-
-const val1 = String(5);
-const val2 = 6;
-const sum = val1 + val2;
-
-console.log(sum);
-console.log(typeof sum);
