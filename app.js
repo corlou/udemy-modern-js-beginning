26ec6e5f208aa7d85bb15980e6fa64ba4b2ec738
Corina Pelichowski <corina@danitt.com>
2020-01-24 22:36:38 +1000

    type conversion

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-// PRIMITIVE
-
-// String
-const name = "John Doe";
-// Number
-const age = 45;
-// Boolean
-const hasKids = false;
-// Null
-const car = null;
-// Undefined
-let test;
-// Symbol
-const sym = Symbol();
+let val;
 
 
-// REFERENCE TYPES
+// Number to String
 
-// Array
-const hobbies = ['movies', 'music',];
-// Object literal
-const address = {
-    city: 'Boston',
-    state: 'MA'
-}
-const today = new Date();
+val = String(5);
+val = String(4 + 4);
+// Boolean to String
+val = String(true);
+// Date to String
+val = String(new Date());
+// Array to String
+val = String([1, 2, 3, 4])
 
-console.log(today);
-console.log(typeof today);
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to Number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1, 2, 3, 4]);
+
+val = parseInt('100.30');
+val = parseFloat('100.3');
+
+// Output 
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed());
+
+const val1 = String(5);
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
