c70e0bf620ce85e5506152b3ddb39c8fce352ffe
Corina Pelichowski <corina@danitt.com>
2020-01-27 17:47:09 +1000

    Arrays and Array Methods

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
-const name = 'John';
-const age = 30;
-const job = 'Web Developer';
-const city = "Miami";
-let html;
+// Create some Arrays
 
-// without template strings (es5)
-html = '<ul><li>Name: ' + name + ' </li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
 
-html = '<ul>' +
-    '<li>Name: ' + name + '</li>' +
-    '<li>Age: ' + age + '</li>' +
-    '<li>Job: ' + job + '</li>' +
-    '<li>City: ' + city + '</li>'
-'</ul>';
 
-function hello() {
-    return 'hello';
-};
+let val;
 
-// with template strings (es6)
+// Get array length
+val = numbers.length;
+// Check if is array
+val = Array.isArray(numbers);
+// Get single value
+val = numbers[3];
+val = numbers[0];
 
-html = `
-    <ul>
-        <li>Name: ${name}</li>
-        <li>Age: ${age}</li>
-        <li>Job: ${job}</li>
-        <li>City: ${city}</li>
-        <li>${2 + 2}</li>
-        <li>${hello()}</li>
-        <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+// Insert into array
+numbers[2] = 100;
+// find index of value
+val = numbers.indexOf(36);
 
-    </ul>
-`;
+// Mutating arrays
+// Add on to end
+//numbers.push(250);
+// Add on to front
+//numbers.unshift(120);
+// Take off from end
+//numbers.pop();
+// Take off from front
+//numbers.shift();
+// Splice values
+//numbers.splice(1, 3);
+// Reverse the array
+//numbers.reverse();
 
-document.body.innerHTML = html;
+// Concatenate array
+val = numbers.concat(numbers2);
+
+// Sorting arrays
+val = fruit.sort();
+//val = numbers.sort();
+
+// Use the "compare function"
+//val = numbers.sort(function (x, y) {
+//    return x - y;
+//});
+
+// Reverse sort
+//val = numbers.sort(function (x, y) {
+//   return y - x;
+//});
+
+// Find
+function under50(num) {
+    return num < 50;
+}
+
+val = numbers.find(under50);
+
+console.log(numbers);
+console.log(val);
