453eb90adf8a97d89813baceeafc9aeda70a9f4f
Corina Pelichowski <corina@danitt.com>
2020-01-28 13:26:12 +1000

    Object Literals

--- app.js
+++ app.js
@@ -1,63 +1,39 @@
-// Create some Arrays
-
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22, 45, 33, 76, 54);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
-
+const person = {
+    firstName: "Steve",
+    lastName: "Smith",
+    age: 32,
+    email: "steve@aol.com",
+    hobbies: ["music", "sports"],
+    address: {
+        city: "Miami",
+        state: "FL"
+    },
+    getBirthYear: function () {
+        return 2020 - this.age;
+    }
+}
 
 let val;
 
-// Get array length
-val = numbers.length;
-// Check if is array
-val = Array.isArray(numbers);
-// Get single value
-val = numbers[3];
-val = numbers[0];
+val = person;
+// Get specific value
+val = person.lastName;
+val = person["lastName"];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address["city"];
+val = person.getBirthYear();
 
-// Insert into array
-numbers[2] = 100;
-// find index of value
-val = numbers.indexOf(36);
-
-// Mutating arrays
-// Add on to end
-//numbers.push(250);
-// Add on to front
-//numbers.unshift(120);
-// Take off from end
-//numbers.pop();
-// Take off from front
-//numbers.shift();
-// Splice values
-//numbers.splice(1, 3);
-// Reverse the array
-//numbers.reverse();
-
-// Concatenate array
-val = numbers.concat(numbers2);
-
-// Sorting arrays
-val = fruit.sort();
-//val = numbers.sort();
-
-// Use the "compare function"
-//val = numbers.sort(function (x, y) {
-//    return x - y;
-//});
-
-// Reverse sort
-//val = numbers.sort(function (x, y) {
-//   return y - x;
-//});
-
-// Find
-function under50(num) {
-    return num < 50;
-}
-
-val = numbers.find(under50);
-
-console.log(numbers);
 console.log(val);
+
+const people = [
+    { name: "John", age: 30 },
+    { name: "Mike", age: 23 },
+    { name: "Nancy", age: 40 }
+];
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].name);
+
+}
