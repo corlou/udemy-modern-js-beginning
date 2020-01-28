67a3672368c20ae84e0ed6fff96076a5138ac7ce
Corina Pelichowski <corina@danitt.com>
2020-01-28 18:26:25 +1000

    Function Declarations  & Expressions

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const color = "red";
+// FUNCTION DECLARATIONS
 
-switch (color) {
-    case "red":
-        console.log("Color is red");
-        break;
-    case "blue":
-        console.log("Color is blue");
-        break;
-    default:
-        console.log("Color is not red or blue");
-        break;
+function greet(firstName = "John", lastName = "Smith") {
+    //console.log("Hello");
+    return "Hello " + firstName + ' ' + lastName;
 }
 
-let day;
+//console.log(greet());
 
-switch (new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-        break;
+// FUNCTION EXPRESSIONS
+const square = function (x = 3) {
+    return x * x;
+};
+
+// console.log(square());
+
+// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFIEs
+// (function () {
+//     console.log("IIFIE Ran...");
+// })();
+
+// (function (name) {
+//     console.log("Hello " + name);
+// })("Brad");
+
+// PROPERTY METHODS
+const todo = {
+    add: function () {
+        console.log("Add todo..");
+    },
+    edit: function (id) {
+        console.log(`Edit todo ${id}`);
+    }
 }
 
-console.log(`Today is ${day}`);
+todo.delete = function () {
+    console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
